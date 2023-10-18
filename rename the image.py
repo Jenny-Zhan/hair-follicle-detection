4b8788e499de650670d4f3ed3f0cdd71fc6451e9
Jenny-Zhan <79676303+Jenny-Zhan@users.noreply.github.com>
2023-10-18 17:58:59 +0800

    Update rename the image.py

--- rename the image.py
+++ rename the image.py
@@ -1,12 +1,15 @@
-# 用户: Jenny
-# 时间: 2021/7/29 13:58
+# user: Jenny
+# time: 2021/7/29 13:58
 import os
 
+# this program is to add a suffix '_t' to the name of target images
+
+# indicate the directory of target images and show the name of target images
 dir_name = "80image_target"
 list = os.listdir(dir_name)
 print(list)
 
-# 请检查文件夹中文件命名数字位数是否相同
+# add suffix and make sure the number in the name is two digits
 for n in range(0, 10):
     os.rename(f"{dir_name}/{list[n]}", f"{dir_name}/0{n}_t.jpg")
 for n in range(10, len(list)):
