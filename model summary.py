e18e663943ffd1d3d3fe303f3b2d12f7312dd8a9
Yichen Zhan <79676303+Jenny-Zhan@users.noreply.github.com>
2023-10-19 10:33:55 +0800

    Update model summary.py

--- model summary.py
+++ model summary.py
@@ -1,6 +1,7 @@
-# 用户: Jenny
-# 时间: 2021/7/31 10:46
+# user: Jenny
+# time: 2021/7/31 10:46
 import keras
 
+# load and present the segmentation model
 conv_autoencoder = keras.models.load_model("se_seg_hairfollicle.h5")
 print(conv_autoencoder.summary())
