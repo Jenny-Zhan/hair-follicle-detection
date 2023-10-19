1e08d0904906510645bf17cf70065ed3a9d79951
Yichen Zhan <79676303+Jenny-Zhan@users.noreply.github.com>
2023-10-19 10:45:05 +0800

    Update process the test photo.py

--- process the test photo.py
+++ process the test photo.py
@@ -1,13 +1,16 @@
-# 用户: Jenny
-# 时间: 2022/5/2 16:04
+# user: Jenny
+# time: 2022/5/2 16:04
 
 from keras_preprocessing import image
 
-# 从PUT PHOTO HERE导入图片
+#this program process and convert the test photo into np.array 
+
+# import the image from directory 'PUT PHOTO HERE'
 img = image.load_img("PUT PHOTO HERE/patient.jpg", color_mode="grayscale")
 img_array = image.img_to_array(img)
 print(img_array.shape)
-# 裁剪图片patient1_1.jpg
+
+# crop the image patient.jpg
 height = img_array.shape[0]
 width = img_array.shape[1]
 a = 0
